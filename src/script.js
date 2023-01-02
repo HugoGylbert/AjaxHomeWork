3a6ad51a32d1ca8e070809862286fed65579938a
HugoGylbert <verokbalazs@gmail.com>
2023-01-03 00:15:47 +0100

    create connetion to api

--- src/script.js
+++ src/script.js
@@ -1,2 +1,37 @@
-const mydata= fetch("https://reqres.in/api/users/1",{method:"GET"}).then((data)=>{console.log(data.json())})
-console.log(mydata)
+function getData(API = "https://reqres.in/api/users/88") {
+  fetch(API, {method: "GET"})
+    .then(checkErrorsApi)
+    .then((a) => {
+      console.log(a);
+    })
+    .catch(TopinfoFlag);
+}
+function checkErrorsApi(data) {
+  switch (data.status) {
+    case 404:
+       throw new Error("Nem található a keresett cím: " + data.url);
+           break;
+    case 500:
+      throw new Error("Server Error");
+      break;
+    default:
+      return data.json();
+  }
+}
+function TopinfoFlag(e) {
+  const alert = document.getElementById("Alert");
+  const text = e.toString()
+  
+  if (text.indexOf("Error") ===  0) {
+    alert.classList.remove();
+    alert.classList.add("alert", "alert-danger");
+  } else {
+    alert.classList.remove();
+    alert.classList.add("alert", "alert-info");
+  }
+  alert.innerText = e;
+  setTimeout(() => {
+    alert.innerText = "";
+    alert.style.display = "none";
+  }, 10 * 1000);
+}
