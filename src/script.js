05a8a3086c6ad313eb8bbf4d71b8c68a62f37111
HugoGylbert <120120941+HugoGylbert@users.noreply.github.com>
2023-01-04 19:56:56 +0100

    new create method

--- src/script.js
+++ src/script.js
@@ -3,7 +3,6 @@ function getData(API = "https://reqres.in/api/users?page=1") {
     .then(checkErrorsApi)
     .then(showContent)
     .catch(TopinfoFlag);
-    
 }
 function checkErrorsApi(data) {
   switch (data.status) {
@@ -39,28 +38,55 @@ function TopinfoFlag(e) {
 }
 function showContent(data) {
   const tableBody = document.getElementById("tbody");
-  const buttonTD = document.createElement("td")
-  const delbutton = document.createElement("button")
+
   for (let c of data.data) {
-    createContentHtml(c)
+    tableBody.appendChild(createContentHtml(c, data));
 
-    const tableRow = document.createElement("tr");
-    const tableRowHead = document.createElement("th");
-    tableRow.appendChild(tableRowHead);
-    tableRowHead.scope = "row";
-
-    tableBody.appendChild(tableRow);
-    tableRow.appendChild(document.createElement("td")).innerHTML = `<img src=${c.avatar}
-     class="img-fluid img-thumbnail rounded-circle" height=auto width=75vw > `;
+    /*.innerHTML = `<img src=${c.avatar}
+     class="img-fluid img-thumbnail rounded-circle" > `;
     tableRow.appendChild(document.createElement("td")).innerHTML =`<b> ${c.first_name} ${c.last_name} </b><br> <i>  <a href="mailto:"> ${c.email}</a></i>`;
     
-    tableRowHead.innerHTML =`# ${c.id} `;
+    tableRowHead.innerHTML =`# ${c.id} `;*/
   }
-
-  
 }
 
-function createContentHtml (w)
-{
-console.log(w)
+function createContentHtml(w) {
+ 
+  const tableRow = document.createElement("tr");
+  const tableRowHead = document.createElement("th");
+  const img = document.createElement("img");
+  const delbutton = document.createElement("button");
+
+  tableRow.appendChild(tableRowHead);
+  tableRowHead.scope = "row";
+
+  const Id = tableRowHead;
+  const pic = tableRow.appendChild(document.createElement("td"));
+  const name = tableRow.appendChild(document.createElement("td"));
+  const email = tableRow.appendChild(document.createElement("td"));
+  const button = tableRow.appendChild(document.createElement("td"));
+
+  delbutton.classList.add("btn","btn-sm" ,"btn-danger");
+  delbutton.innerText = "Delete";
+
+  button.appendChild(delbutton);
+  Id.innerHTML = "# " + w.id;
+  pic.appendChild(img);
+  img.src = `${w.avatar}`;
+  img.width = 100;
+  img.classList.add("img-fluid", "img-thumbnail", "rounded-circle");
+
+  name.innerHTML = "<b>" + w.first_name + " " + w.last_name + "</b>";
+  name.appendChild(document.createElement("br"));
+  name.innerHTML += "<i> <a href=mailto: > " + w.email + "</a> </i>";
+  name.firstChild.style.cursor = "pointer";
+
+
+  name.firstChild.onclick = () => {};
+
+  delbutton.onclick=()=>{console.log("del")}
+
+  email.innerHTML;
+
+  return tableRow;
 }
