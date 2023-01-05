aa58c288de9227201a34b3f86dbb1c000fb064b7
HugoGylbert <verokbalazs@gmail.com>
2023-01-05 12:55:58 +0100

    new features

--- src/script.js
+++ src/script.js
@@ -1,9 +1,26 @@
-function getData(API = "https://reqres.in/api/users?page=1") {
-  fetch(API, { method: "GET" })
-    .then(checkErrorsApi)
-    .then(showContent)
-    .catch(TopinfoFlag);
+
+
+function getData(API = "https://reqres.in/api/users/") {
+    document.getElementById("placeholder").style.display = "block"
+  fetch(API, {method: "GET"}).then(checkErrorsApi).then(showContent).catch(TopinfoFlag);
 }
+function deleteData (w){
+    document.getElementById("placeholder").style.display = "block"
+    const DeleteUrl = `https://reqres.in/api/users/${w.id}`;
+    fetch(DeleteUrl, {method: "DELETE"})
+      .then((res) => {
+        if (res.status !== 204) {
+          throw new Error("Something went wrong, try it later.");
+        } else {
+           TopinfoFlag((`${w.first_name} ${w.last_name}'s data deleted!`),5)
+           document.getElementById("tbody").innerHTML=""
+           getData()
+        }
+      })
+      .catch(TopinfoFlag);
+   
+  };
+
 function checkErrorsApi(data) {
   switch (data.status) {
     case 404:
@@ -19,26 +36,31 @@ function checkErrorsApi(data) {
       return data.json();
   }
 }
-function TopinfoFlag(e) {
-  const alert = document.getElementById("Alert");
+function TopinfoFlag(e, t = 10) {
+    const alert = document.getElementById("Alert");
+    alert.style.visibility="visible"
+    alert.style.display="block"
   const text = e.toString();
 
   if (text.indexOf("Error") === 0) {
     alert.classList.remove();
-    alert.classList.add("alert", "alert-danger");
+    alert.classList.add("alert","alert-danger","sticky-top" );
   } else {
     alert.classList.remove();
-    alert.classList.add("alert", "alert-info");
+    alert.classList.add("alert", "alert-info","sticky-top");
   }
   alert.innerText = e;
   setTimeout(() => {
     alert.innerText = "";
-    alert.style.display = "none";
-  }, 10 * 1000);
+    alert.style.visibility="hidden"
+  alert.style.display="none"
+  }, t * 1000);
+  
 }
 function showContent(data) {
   const tableBody = document.getElementById("tbody");
 
+  const placeholder = (document.getElementById("placeholder").style.display = "none");
   for (let c of data.data) {
     tableBody.appendChild(createContentHtml(c, data));
 
@@ -51,7 +73,6 @@ function showContent(data) {
 }
 
 function createContentHtml(w) {
- 
   const tableRow = document.createElement("tr");
   const tableRowHead = document.createElement("th");
   const img = document.createElement("img");
@@ -66,27 +87,78 @@ function createContentHtml(w) {
   const email = tableRow.appendChild(document.createElement("td"));
   const button = tableRow.appendChild(document.createElement("td"));
 
-  delbutton.classList.add("btn","btn-sm" ,"btn-danger");
+  delbutton.classList.add("btn", "btn-sm", "btn-danger");
   delbutton.innerText = "Delete";
 
   button.appendChild(delbutton);
+  button.classList.add("align-middle");
+
   Id.innerHTML = "# " + w.id;
+  Id.classList.add("align-middle");
+  Id.style.fontSize = "xx-small";
+
   pic.appendChild(img);
   img.src = `${w.avatar}`;
-  img.width = 100;
   img.classList.add("img-fluid", "img-thumbnail", "rounded-circle");
+  img.width = 75;
 
+  name.classList.add("align-middle");
   name.innerHTML = "<b>" + w.first_name + " " + w.last_name + "</b>";
   name.appendChild(document.createElement("br"));
   name.innerHTML += "<i> <a href=mailto: > " + w.email + "</a> </i>";
   name.firstChild.style.cursor = "pointer";
 
+  name.firstChild.onclick = () => {
+    createCard(w);
+  };
 
-  name.firstChild.onclick = () => {};
+  delbutton.onclick = (e) => {
+    RUSureDelete(w);
+  };
 
-  delbutton.onclick=()=>{console.log("del")}
 
-  email.innerHTML;
 
   return tableRow;
+}
+function createCard(w) {
+
+const body = document.body
+
+
+const Carddiv =document.getElementById("ModalCard")
+
+const CarddivContainer =document.getElementById("ModalCardContainer")
+const CarddivBody =document.getElementById("CardModalBody")
+const ModalCardDiv = new bootstrap.Modal(document.getElementById("ModalCard"))
+const cardTitle = document.getElementById("cardTitle")
+cardTitle.innerText= `${w.first_name} ${w.last_name} `
+const pic = document.getElementById("modalCardPic")
+
+pic.src=`${w.avatar}`
+
+
+
+
+
+ModalCardDiv.show()
+console.log(ModalCardDiv._isShown)
+
+}
+function RUSureDelete(w) {
+  const myModal = new bootstrap.Modal(document.getElementById("staticBackdrop"));
+  myModal.show();
+  const ModalTitle = document.getElementById("staticBackdropLabel");
+  const ModalBody = document.getElementById("ModalBody");
+  const ModalDelete = document.getElementById("ModalDelete");
+
+  ModalTitle.innerText = "Are you sure you want to delete?";
+  ModalBody.innerText = `${w.first_name} ${w.last_name}'s all data will be delete! `;
+  ModalDelete.onclick = () => {
+    myModal.hide();
+    deleteData(w)
+  }
+
+  
+
+  return false;
 }
